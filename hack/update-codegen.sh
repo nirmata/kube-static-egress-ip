5d54c17cecb9e023e45c224028b51dacbfee6ebd
Murali Reddy <muralimmreddy@gmail.com>
2018-11-28 00:20:06 +0530

    generated code

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -28,7 +28,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/nirmata/kube-static-egress-ip/pkg/client github.com/nirmata/kube-static-egress-ip/pkg/apis \
   egressip:v1alpha1 \
-  --output-base "$(dirname ${BASH_SOURCE})/../../.." \
+  --output-base "$(dirname ${BASH_SOURCE})/../../../.." \
   --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
 
 # To use your own boilerplate text use:
